08c5f81e7e38d7d2d78978af504d1e53356adba2
vuvietcuong5806-ai <vuvietcuong5806@gmail.com>
2026-06-05 22:59:26 +0700

    hoan thanh

--- bttuan13.cpp
+++ bttuan13.cpp
@@ -24,3 +24,16 @@ Node* search(Node* goc, int date){
 	else
 	return search(goc->phai, date);
 } 
+
+int main(){
+	int data[]={2001, 2002, 2006, 2007,2003, 2004,2005, 2001, 1999, 2004};
+	int n=sizeof(data) / sizeof(data[0]);
+	int datecantim;
+	cin>>datecantim;
+	Node* goc=nullptr;
+	for(int i=0; i<n; i++) goc=insert(goc, data[i]);
+	Node* Kq= search(goc, datecantim);
+	if(Kq) cout<<"Da tim thay sinh vien co nam sinh: "<<Kq->date;
+	else cout<<"Khong tim thay"<<endl;
+	return 0;
+} 
