617847b27cbc8cd4482ca716432fb4d01b756164
vuvietcuong5806-ai <vuvietcuong5806@gmail.com>
2026-06-18 01:00:13 +0700

    Add files via upload

--- bttuan14.3.cpp
+++ bttuan14.3.cpp
@@ -19,4 +19,51 @@ string city[MAX] = {
     "Uong Bi"
 };
 
+list<int> adj[MAX];
+void addEdge(int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
 
+void BFS(int start)
+{
+    bool visited[MAX] = {false};
+    queue<int> q;
+    visited[start] = true;
+    q.push(start);
+    while(!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        cout << city[u] << endl;
+        for(int v : adj[u])
+        {
+            if(!visited[v])
+            {
+                visited[v] = true;
+                q.push(v);
+            }
+        }
+    }
+}
+
+int main()
+{
+    addEdge(0,1);
+    addEdge(0,2);
+    addEdge(0,3);
+    addEdge(0,4);
+    addEdge(0,5);
+    addEdge(0,6);
+    addEdge(1,7);
+    addEdge(1,8);
+    addEdge(2,7);
+    addEdge(6,9);
+    addEdge(6,10);
+    addEdge(9,10);
+    addEdge(8,10);
+    cout << "BFS tu Ha Noi:\n\n";
+    BFS(0);
+    return 0;
+}
