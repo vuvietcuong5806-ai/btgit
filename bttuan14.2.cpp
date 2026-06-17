7503c41957b450a31bf18c4232dd26e80f3a2e94
vuvietcuong5806-ai <vuvietcuong5806@gmail.com>
2026-06-18 00:02:51 +0700

    hoan thanh

--- bttuan14.2.cpp
+++ bttuan14.2.cpp
@@ -17,4 +17,51 @@ string city[MAX] = {
     "Bac Giang",
     "Uong Bi"
 };
+int a[MAX][MAX]={0};
+void addEdge(int u, int v)
+{
+    a[u][v]=1;
+    a[v][u]=1;
+}
 
+void BFS(int start)
+{
+    bool visited[MAX]={false};
+    queue<int> q;
+    visited[start]=true;
+    q.push(start);
+    while(!q.empty())
+    {
+        int u=q.front();
+        q.pop();
+        cout <<city[u]<< endl;
+        for(int v=0;v<MAX; v++)
+        {
+            if(a[u][v]==1&&!visited[v])
+            {
+                visited[v]=true;
+                q.push(v);
+            }
+        }
+    }
+}
+
+int main()
+{
+    addEdge(0,1);
+    addEdge(0,2);
+    addEdge(0,3);
+    addEdge(0,4);
+    addEdge(0,5);
+    addEdge(0,6);
+    addEdge(1,7);
+    addEdge(1,8);
+    addEdge(2,7);
+    addEdge(6,9);
+    addEdge(6,10);
+    addEdge(9,10);
+    addEdge(8,10);
+    cout <<"BFS tu Ha Noi:\n\n";
+    BFS(0);
+    return 0;
+}
