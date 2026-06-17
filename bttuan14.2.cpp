ac31f9fe29612a11bfb29e0a144cea74619631b3
vuvietcuong5806-ai <vuvietcuong5806@gmail.com>
2026-06-17 23:04:55 +0700

    Add files via upload

---
+++ bttuan14.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define MAX 11
+
+string city[MAX] = {
+    "Ha Noi",
+    "Hai Duong",
+    "Phu Ly",
+    "Hoa Binh",
+    "Son Tay",
+    "Thai Nguyen",
+    "Bac Ninh",
+    "Hung Yen",
+    "Hai Phong",
+    "Bac Giang",
+    "Uong Bi"
+};
+
