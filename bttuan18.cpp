be0488194d8609ca55a1d23971e34fc82b1e48dc
vuvietcuong5806-ai <vuvietcuong5806@gmail.com>
2026-06-29 22:59:24 +0700

    krusal alg

--- bttuan18.cpp
+++ bttuan18.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-#include <string>
-#include <queue>
-#include <iomanip>
+#include <algorithm>
 using namespace std;
 
-const int Sotinh_max = 11;
-const int INF = 1e9;    
+const int V = 11;
+string ten[V] = {"Ha Noi","Son Tay","Hoa Binh","Phu Ly","Hung Yen",
+                 "Hai Duong","Hai Phong","Uong Bi","Bac Ninh","Bac Giang","Thai Nguyen"};
 
-const string DSTinh[Sotinh_max] = { "HN", "TN", "BN", "BG", "UB", "HP", "HD", "HY", "PL", "HB", "ST" }; 
-
-struct Matran {   // khai báo
-    int Sotinhxet;
-    int matrix[Sotinh_max][Sotinh_max];
+struct Canh { int u, v, w; };
+Canh canh[] = {
+    {0,5,1},{5,4,2},{4,3,3},{0,3,4},{0,2,5},
+    {0,1,6},{0,10,7},{0,8,8},{8,9,9},{9,7,10},
+    {8,7,11},{7,6,12},{5,6,13}
 };
+const int E = 13;
+
+int cha[V];
+int find(int x) { return cha[x] == x ? x : cha[x] = find(cha[x]); }
+
 
